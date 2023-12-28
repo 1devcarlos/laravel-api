e45d4b2568334a4b6b01549ca1ff8abe4a899de8
Carlos Gomes <cg.dev1@hotmail.com>
2023-12-28 16:44:10 -0300

    Data fetched from quizapi, and showed in inicial route

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Question;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class QuestionController extends Controller
+{
+    public function fetchInsert() {
+        $response = Http::get('https://quizapi.io/api/v1/questions', [
+            'apiKey'=> 'GR1LWhjKZPm10xGvLFsZyghLjQrB2UeqSPtVuU8L',
+            'limit'=> 10,
+        ]);
+        $questions = json_decode($response->body());
+        foreach($questions as $q){
+            $question = new Question();
+    
+            $question -> question = $q -> question; 
+            $question -> answer_a = $q -> answers -> answer_a;
+            $question -> answer_b = $q -> answers -> answer_b;
+            $question -> answer_c = $q -> answers -> answer_c;
+            $question -> save();
+        }
+
+        return "Data fecthed from external api and saved into Database!";
+    }
+
+    public function show() {
+        $data['questions'] = Question::all();
+        return view('welcome', $data);
+    }
+}
